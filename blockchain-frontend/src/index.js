7a673024647a6116c61d7c5c9320a2b358fdd579
enthusiastica <williamcambell682@gmail.com>
2022-08-27 13:44:45 +0530

    saving

--- blockchain-frontend/src/index.js
+++ blockchain-frontend/src/index.js
@@ -4,11 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+///////////////////
+
+import ethers from 'ethers';
+
+window.onload = connect;
+
+async function connect() {
+	const { ethereum } = window;
+
+	if (!ethereum) {
+		alert("please install metamask!");
+		return;
+	}
+
+	const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+	const provider = new ethers.providers.Web3Provider(ethereum); // to request metamask
+	const signer = provider.getSigner();
+
+	if (accounts.length === 0) {
+		alert("No Account :(");
+		return;
+	}
+}
+////////////////
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
