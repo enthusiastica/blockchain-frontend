480cf83766c19cf995f1732d80d6769aef859bff
enthusiastica <williamcambell682@gmail.com>
2022-08-27 16:18:26 +0530

    connection done

--- blockchain-frontend/src/App.js
+++ blockchain-frontend/src/App.js
@@ -5,6 +5,11 @@ import Alert from 'react-bootstrap/Alert';
 
 import ciondigital from './ciondigital.png';
 
+
+import { contract } from './index';
+
+
+
 let districts = ["Ahmednagar", "Akola", "Amravati", "Aurangabad", "Beed", "Bhandara", "Buldhana", "Chandrapur", "Dhule", "Gadchiroli", "Gondia", "Hingoli", "Jalgaon", "Jalna", "Kolhapur", "Latur", "Mumbai City", "Mumbai Suburban", "Nagpur", "Nanded", "Nandurbar", "Nashik", "Osmanabad", "Palghar", "Parbhani", "Pune", "Raigad", "Ratnagiri", "Sangli", "Satara", "Sindhudurg", "Solapur", "Thane", "Wardha", "Washim", "Yavatmal"];
 
 function formSubmitted(e) {
--- blockchain-frontend/src/index.js
+++ blockchain-frontend/src/index.js
@@ -6,25 +6,51 @@ import reportWebVitals from './reportWebVitals';
 
 ///////////////////
 
-import ethers from 'ethers';
+import { ethers } from 'ethers';
+import abi from './abi.json' // change it only if backend is updated
+
+const contract_address = "0xaa09D7E6f43d21D6ac5f1DCC9C66cee6Abe56b85"; // add address in single or double quotes ("", '')
 
 window.onload = connect;
+
+var contract;
+var userAddress;
 
 async function connect() {
 	const { ethereum } = window;
 
 	if (!ethereum) {
+		console.log("Metamask not found")
 		alert("please install metamask!");
 		return;
 	}
 
-	const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-	const provider = new ethers.providers.Web3Provider(ethereum); // to request metamask
-	const signer = provider.getSigner();
+	console.log("Requesting account to metamask.....")
 
-	if (accounts.length === 0) {
-		alert("No Account :(");
-		return;
+	try {
+		const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+		const provider = new ethers.providers.Web3Provider(ethereum); // to request metamask
+		const signer = provider.getSigner();
+
+		if (accounts.length === 0) {
+			alert("No Account :(");
+			return;
+		}
+
+		userAddress = accounts[0];
+		console.log(userAddress); // getting address of logged in user
+
+		contract = new ethers.Contract(contract_address, abi, signer); // creating reference to blockchain contract
+		console.log(contract);
+
+		// let op1 = await contract.setDay(1);
+		let op2 = await contract.c_day();
+		// console.log("setDay: ", op1);
+		console.log("c_day : ", op2);
+	} catch (e) {
+		// alert(e)
+		console.log("error: ", e);
 	}
 }
 ////////////////
@@ -35,6 +61,8 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
+
+export { contract, userAddress }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
